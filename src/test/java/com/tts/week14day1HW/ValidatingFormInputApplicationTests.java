384fbd2a35368453c74831ae59d0ebd424c59f1b
MarquisBlocker <mblocker108@rams.wssu.edu>
2021-05-17 15:59:06 -0400

    Initial commit

---
+++ src/test/java/com/tts/week14day1HW/ValidatingFormInputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.week14day1HW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingFormInputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
